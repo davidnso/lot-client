443045ce9dbf8f1f1a58798a630fa3daa7b178ac
davidnso <davidnsoesie1@gmail.com>
2020-03-05 17:50:23 -0500

    add featured sellers to the landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThumbnailItemComponent } from './shared/thumbnail-item/thumbnail-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThumbnailItemComponent
   ],
   imports: [
     BrowserModule,
