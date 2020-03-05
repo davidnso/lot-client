b2bfed4e62515332f68b35a51ffd6064fdba6454
davidnso <davidnsoesie1@gmail.com>
2020-03-05 17:50:40 -0500

    add base thumbnail item

---
+++ src/app/shared/thumbnail-item/thumbnail-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thumbnail-item',
+  templateUrl: './thumbnail-item.component.html',
+  styleUrls: ['./thumbnail-item.component.scss']
+})
+export class ThumbnailItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
