13ec95381a8966d558d51d838f27990f84f0c6af
davidnso <davidnsoesie1@gmail.com>
2020-03-08 21:21:53 -0400

    :construction: create auth component for login and registration

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
+  continueClicked: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  continue(){
+    this.continueClicked = true;
+  }
 }
