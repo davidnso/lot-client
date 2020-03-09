01350e289dbec7bba002af83aaf290c6ee4d148c
davidnso <davidnsoesie1@gmail.com>
2020-03-08 21:23:51 -0400

    add dashboard to the lot component.

--- src/app/lot/lot.module.ts
+++ src/app/lot/lot.module.ts
@@ -5,16 +5,13 @@ import { LOTRoutingModule } from './lot-routing.module';
 import { PlugComponent } from './plug/plug.component';
 import { StoreComponent } from './dashboards/store/store.component';
 import { CustomerComponent } from './dashboards/customer/customer.component';
-import { StatsComponent } from './dashboards/store/stats/stats.component';
-import { OrdersComponent } from './dashboards/store/orders/orders.component';
-import { ListingsComponent } from './dashboards/store/listings/listings.component';
-import { BuilderComponent } from './dashboards/store/components/builder/builder.component';
-import { BuyOrdersComponent } from './dashboards/store/components/buy-orders/buy-orders.component';
-import { CustomerOrderComponent } from './dashboards/store/components/buy-orders/customer-order/customer-order.component';
+import { StatsComponent } from './dashboards/store/components/stats/stats.component';
+import { OrdersComponent } from './dashboards/store/components/orders/orders.component';
+import { ListingsComponent } from './dashboards/store/components/listings/listings.component';
 
 
 @NgModule({
-  declarations: [PlugComponent, StoreComponent, CustomerComponent, StatsComponent, OrdersComponent, ListingsComponent, BuilderComponent, BuyOrdersComponent, CustomerOrderComponent],
+  declarations: [PlugComponent, StoreComponent, CustomerComponent, StatsComponent, OrdersComponent, ListingsComponent],
   imports: [
     CommonModule,
     LOTRoutingModule
