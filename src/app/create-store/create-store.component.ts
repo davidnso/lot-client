eb56e7f308cb0bac0914cb4e599973ecb3470792
davidnso <davidnsoesie1@gmail.com>
2020-03-28 11:37:05 -0400

    add form controls for creating a store & plug in data layer

--- src/app/create-store/create-store.component.ts
+++ src/app/create-store/create-store.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { DataService } from '../data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { LoginDialogComponent } from '../shared/login-dialog/login-dialog.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-store',
@@ -6,10 +12,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-store.component.scss']
 })
 export class CreateStoreComponent implements OnInit {
+  createStoreForm = new FormGroup({
+    name: new FormControl(''),
+    streetAddress: new FormControl(''),
+    apartment: new FormControl(''),
+    city: new FormControl(''),
+    zipCode: new FormControl(''),
+    state: new FormControl(''),
+    phoneNumber: new FormControl('')
+  });
 
-  constructor() { }
+  userIsLoggedIn = false;
+  constructor(
+    private dataService: DataService,
+    public dialog: MatDialog,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.checkLoginStatus();
   }
 
+  registerNewStore() {
+    const storeRegistrationParams = {
+      name: this.createStoreForm.value.name,
+      bannerImgUrl: 'https://',
+      addresses: [
+        {
+          streetAddress: this.createStoreForm.value.streetAddress as string,
+          streetAddress2: this.createStoreForm.value.apartment as string,
+          city: this.createStoreForm.value.city as string,
+          state: this.createStoreForm.value.state as string,
+          zipCode: this.createStoreForm.value.zipCode as string,
+          country: 'USA'
+        }
+      ],
+      phoneNumber: this.createStoreForm.value.phoneNumber as string,
+      tier: 'first'
+    };
+
+  
+
+    this.dataService
+      .createStore({ requester: JSON.parse(localStorage.getItem('user')).username, storeRegistrationParams })
+      .subscribe((res: any) => {
+        console.log(res)
+        this.dialog.open(ConfirmationComponent, {
+          width: '600px',
+          data: {
+            owner: JSON.parse(localStorage.getItem('user')).name || 'name',
+            name: res.outlet.name,
+            addresses: res.outlet.addresses[0]
+          }
+        });
+    
+        this.dialog.afterAllClosed.subscribe(() => {
+          this.router.navigate(['']);
+        });
+
+      });
+  }
+
+  private checkLoginStatus() {
+    try {
+      const isLoggedIn = localStorage.getItem('user');
+      if (isLoggedIn) {
+        this.userIsLoggedIn = true;
+      } else {
+        this.dialog.open(LoginDialogComponent, { disableClose: true });
+      }
+    } catch (error) {
+      throw error;
+      // TODO: Create component that shows errors...
+    }
+  }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,154 +1,29 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { Injectable } from "@angular/core";
 
 @Injectable({
     providedIn: 'root'
 })
 export class DataService { 
-    mockDataSet = {
-        customer: { 
-            username: 'dnsoes1',
-            email: 'davidnsoesie@place.com',
-            name: 'David Nsoesie',
-            role: 'consumer',
-            password: 'password',
-            addresses: [
-                {
-                    streetAddress: '13613 oaklands manor dr.',
-                    streetAddress2: '',
-                    zipCode: '20708',
-                    city: 'laurel',
-                    state: 'MD',
-                    country: 'USA'
-                }
-            ]
-        },
-        owner: {
-            username: 'mnsoes1',
-            email: 'mikensoes@place.com',
-            name: 'Michael Nsoesie',
-            role: 'owner',
-            password: 'password',
-            addresses: [
-                {
-                    streetAddress: '13613 oaklands manor dr.',
-                    streetAddress2: '',
-                    zipCode: '20708',
-                    city: 'laurel',
-                    state: 'MD',
-                    country: 'USA'
-                }
-            ]
-        },
-        store:{
-            name: 'Mike and Ike',
-            bannerImg: '',
-            addresses: [
-                {
-                    streetAddress: '12323 street dr.',
-                    streetAddress2: '',
-                    zipCode: '20708',
-                    city: 'laurel',
-                    state: 'MD',
-                    country: 'USA'
-                }
-            ],
-            owner: 'mnsoes1',
-            dateCreated: '193921039021',
-            Tier: 'first'
-        },
-        listing: [
-            {
-                name: 'Jordan 5 Off White',
-                description: '',
-                price: '$299.99',
-                brand: 'Jordan',
-                details: {
-                    category: 'shoes',
-                    gender: 'men',
-                    subCategory: 'basketball',
-                    class: '',
-                    stock: [
-                        {
-                            size: '10',
-                            quantity: '300',
-                            color: 'black',
-                        }
-                    ],
-                    condition: 'great',
-                },
-                images: [
-                    '',
-                    ''
-                ],
-                storeId: '',
-                status: 'active',
-                tags: '',
-                date: ''
-            },
-            {
-                name: 'Air Max 95',
-                description: '',
-                price: '$299.99',
-                brand: 'Jordan',
-                details: {
-                    category: 'shoes',
-                    gender: 'men',
-                    subCategory: 'basketball',
-                    class: '',
-                    stock: [
-                        {
-                            size: '10',
-                            quantity: '300',
-                            color: 'black',
-                        }
-                    ],
-                    condition: 'great',
-                },
-                images: [
-                    '',
-                    ''
-                ],
-                storeId: '',
-                status: 'active',
-                tags: '',
-                date: ''
-            },
-            {
-                name: 'Jordan 3 True Blue',
-                description: '',
-                price: '$299.99',
-                brand: 'Jordan',
-                details: {
-                    category: 'shoes',
-                    gender: 'men',
-                    subCategory: 'basketball',
-                    class: '',
-                    stock: [
-                        {
-                            size: '10',
-                            quantity: '300',
-                            color: 'black',
-                        }
-                    ],
-                    condition: 'great',
-                },
-                images: [
-                    '',
-                    ''
-                ],
-                storeId: '',
-                status: 'active',
-                tags: '',
-                date: ''
-            }
-        ]
-    }
 
-    constructor(){
+    constructor(private http: HttpClient) {}
 
-    }
-
-    validateCredentials(){
-
+    createStore(args: {
+        requester: string,
+        storeRegistrationParams: {
+        name: string,
+        bannerImgUrl: string
+        addresses:
+      {
+        streetAddress: string;
+        streetAddress2: string;
+        zipCode: string;
+        city: string;
+        state: string;
+        country: string;
+      }[]
+    }}) {
+       return this.http.post(`${environment.LOT_API}/outlet/${args.requester}`, { info: args.storeRegistrationParams});
     }
 }
