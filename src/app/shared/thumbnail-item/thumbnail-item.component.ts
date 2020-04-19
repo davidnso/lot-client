edaf2a5d66f81ec1ff8023cf933768e2bd7e5678
davidnso <davidnsoesie1@gmail.com>
2020-04-19 19:16:25 -0400

    add index item interface

--- src/app/shared/thumbnail-item/thumbnail-item.component.ts
+++ src/app/shared/thumbnail-item/thumbnail-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-thumbnail-item',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thumbnail-item.component.scss']
 })
 export class ThumbnailItemComponent implements OnInit {
-
+  @Input() data;
   constructor() { }
 
   ngOnInit() {
