1ecbad8a680ee58441cfbdadf510901ac07e37ef
davidnso <davidnsoesie1@gmail.com>
2020-04-19 19:17:08 -0400

    begin dev for search functionality

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
-  styleUrls: ['./browse.component.scss']
+  styleUrls: ['./browse.component.scss'],
 })
 export class BrowseComponent implements OnInit {
-
   activeParam: string;
-
-  constructor(private route: ActivatedRoute) {
-    this.route.queryParams.subscribe(params => this.activeParam = params.category)
+  resultSet = [];
+  constructor(private route: ActivatedRoute, private dataService: DataService) {
+    this.route.queryParams.subscribe(
+      (params) => (this.activeParam = params.category)
+    );
   }
 
   ngOnInit() {
+    this.searchForListings();
   }
 
+  searchForListings() {
+    try {
+      this.dataService
+        .searchListings({ query: {} })
+        .subscribe((dataset: any[]) => {
+          this.resultSet = dataset;
+          console.log(this.resultSet)
+        });
+    } catch (error) {
+      throw error;
+    }
+  }
 }
